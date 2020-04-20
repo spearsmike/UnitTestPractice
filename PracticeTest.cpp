7039456390f43d4c68400ef2e34191937a09f7c2
Michael R Spears <mspears@mail.csuchico.edu>
2020-04-20 16:45:44 -0700

    Unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -19,4 +19,49 @@ TEST(PracticeTest, is_simple_palindrome)
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+	ASSERT_TRUE(obj.isPalindrome(""));
+	ASSERT_TRUE(obj.isPalindrome("a"));
+	ASSERT_TRUE(obj.isPalindrome("BaB"));
+	ASSERT_TRUE(obj.isPalindrome("BAAB"));
+	ASSERT_TRUE(obj.isPalindrome("\0\0"));
+	ASSERT_TRUE(obj.isPalindrome("  "));
+	ASSERT_TRUE(obj.isPalindrome(" "));
+	ASSERT_TRUE(obj.isPalindrome("a  a"));
+}
+
+TEST(PracticeTest, is_not_a_palindrome)
+{
+	Practice obj;
+	ASSERT_FALSE(obj.isPalindrome("ab"));
+	ASSERT_TRUE(obj.isPalindrome("Bab"));
+	ASSERT_TRUE(obj.isPalindrome("Aa"));
+	ASSERT_FALSE(obj.isPalindrome("abc12cba"));
+}
+
+TEST(PracticeTest, sort_decending)
+{
+	Practice obj;
+	int a;
+	int b;
+	int c;
+	obj.sortDescending(a=3, b=2, c=1);
+		ASSERT_EQ(a, 1);
+		ASSERT_EQ(b, 2);
+		ASSERT_EQ(c, 3);
+	obj.sortDescending(a=1, b=2, c=3);
+		ASSERT_EQ(a, 1);
+		ASSERT_EQ(b, 2);
+		ASSERT_EQ(c, 3);
+	obj.sortDescending(a=1, b=1, c=1);
+		ASSERT_EQ(a, 1);
+		ASSERT_EQ(b, 1);
+		ASSERT_EQ(c, 1);
+	obj.sortDescending(a=0, b=0, c=0);
+		ASSERT_EQ(a, 0);
+		ASSERT_EQ(b, 0);
+		ASSERT_EQ(c, 0);
+	obj.sortDescending(a=1, b=0, c=1);
+		ASSERT_EQ(a, 1);
+		ASSERT_EQ(b, 1);
+		ASSERT_EQ(c, 0);
 }
